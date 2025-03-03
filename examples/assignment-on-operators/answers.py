f919acba45fe589243c6b03844cf732894e38148
poojitha <poojithadspr@gmail.com>
2025-03-03 14:36:50 +0530

    answers task 3

--- examples/assignment-on-operators/answers.py
+++ examples/assignment-on-operators/answers.py
@@ -11,3 +11,22 @@ print ("sum value is:", sum)
 print ("diff is:", difference)
 print ("product is:", product)
 print("quotient is :", quotient)
+
+#TASK2 COMPARISON OPERATORS
+
+print(f"{a} < {b} -> {a < b}")
+print(f"{a} > {b} -> {a > b}")
+print(f"{a} >= {b} -> {a >= b}")
+print(f"{a} >= {b} -> {a >= b}")
+print(f"{a} <= {b} -> {a <= b}")
+print(f"{a} == {b} -> {a == b}")
+print(f"{a} != {b} -> {a != b}")
+
+#task 3 
+
+x = True
+y = False
+print(f"x and y -> {x and y}")  # Logical AND
+print(f"x or y  -> {x or y}")   # Logical OR
+print(f"not x   -> {not x}")    # Logical NOT
+print(f"not y   -> {not y}") 
