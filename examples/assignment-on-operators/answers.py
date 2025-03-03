814a5a74c73b58aab59e2449162acdac4c8cd681
poojitha <poojithadspr@gmail.com>
2025-03-03 14:14:18 +0530

    answers task 1

---
+++ examples/assignment-on-operators/answers.py
@@ -0,0 +1,13 @@
+
+#TASK1
+a = 10
+b = 5
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a/b
+
+print ("sum value is:", sum)
+print ("diff is:", difference)
+print ("product is:", product)
+print("quotient is :", quotient)
